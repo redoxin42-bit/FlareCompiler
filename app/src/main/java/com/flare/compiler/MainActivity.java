0f23bba868d35dd15a3451f944fe712edbec3765
redoxin42-bit <redoxin42@gmail.com>
2026-06-21 15:52:35 +0300

    Update Java Code from Bot

--- app/src/main/java/com/flare/compiler/MainActivity.java
+++ app/src/main/java/com/flare/compiler/MainActivity.java
@@ -1,1 +1,15 @@
-test
+package com.flare.compiler;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView textView = new TextView(this);
+        textView.setText("Hello World from FlareBuilder!");
+        setContentView(textView);
+    }
+}
