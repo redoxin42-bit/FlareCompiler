27c70a4ddf43d22eec41de7aceba8be9a87c0428
redoxin42-bit <redoxin42@gmail.com>
2026-06-21 18:59:40 +0300

    Update MainActivity.kt

--- app/src/main/kotlin/com/flare/compiler/MainActivity.kt
+++ app/src/main/kotlin/com/flare/compiler/MainActivity.kt
@@ -3,12 +3,27 @@ package com.flare.compiler
 import android.os.Bundle
 import android.app.Activity
 import android.widget.TextView
+import android.widget.Button
+import android.widget.LinearLayout
+import android.view.ViewGroup
+import android.widget.LinearLayout.LayoutParams
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val linearLayout = LinearLayout(this)
+        linearLayout.orientation = LinearLayout.VERTICAL
+        linearLayout.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+
         val textView = TextView(this)
         textView.text = "Hello World from FlareBuilder Kotlin IDE!"
-        setContentView(textView)
+        linearLayout.addView(textView)
+
+        val deleteButton = Button(this)
+        deleteButton.text = "Удалить"
+        linearLayout.addView(deleteButton)
+
+        setContentView(linearLayout)
     }
 }
