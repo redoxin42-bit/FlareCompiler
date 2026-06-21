eef47bdcbd614df1953517e36210bfed7da036f2
redoxin42-bit <redoxin42@gmail.com>
2026-06-21 19:15:39 +0300

    Create app/src/main/kotlin/flare/client/app/data/model/SubscriptionEntity.kt

---
+++ app/src/main/kotlin/flare/client/app/data/model/SubscriptionEntity.kt
@@ -0,0 +1,21 @@
+package flare.client.app.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "subscriptions")
+data class SubscriptionEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String,
+    val url: String,
+    val upload: Long = 0,
+    val download: Long = 0,
+    val total: Long = 0,
+    val expire: Long = 0,
+    val description: String = "",
+    val supportUrl: String = "",
+    val webPageUrl: String = "",
+    val updateInterval: Long = 0,
+    val lastUpdated: Long = 0,
+    val pinned: Long = 0L
+)
