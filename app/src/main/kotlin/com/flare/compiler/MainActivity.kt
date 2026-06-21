08b834b2b61537b5fd1ac8e276e1d149dac733a4
redoxin42-bit <redoxin42@gmail.com>
2026-06-21 19:29:54 +0300

    Update app/src/main/kotlin/com/flare/compiler/MainActivity.kt

--- app/src/main/kotlin/com/flare/compiler/MainActivity.kt
+++ app/src/main/kotlin/com/flare/compiler/MainActivity.kt
@@ -2,28 +2,19 @@ package com.flare.compiler
 
 import android.os.Bundle
 import android.app.Activity
-import android.widget.TextView
-import android.widget.Button
-import android.widget.LinearLayout
-import android.view.ViewGroup
-import android.widget.LinearLayout.LayoutParams
+// import android.widget.TextView // No longer needed as we're using an XML layout
+// import android.widget.Button // Not strictly needed unless adding a click listener
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Set the content view to our new XML layout
+        setContentView(R.layout.activity_main)
 
-        val linearLayout = LinearLayout(this)
-        linearLayout.orientation = LinearLayout.VERTICAL
-        linearLayout.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-
-        val textView = TextView(this)
-        textView.text = "Hello World from FlareBuilder Kotlin IDE!"
-        linearLayout.addView(textView)
-
-        val deleteButton = Button(this)
-        deleteButton.text = "Удалить"
-        linearLayout.addView(deleteButton)
-
-        setContentView(linearLayout)
+        // If you needed to interact with the button later, you would do:
+        // val flareTradeButton: Button = findViewById(R.id.flareTradeButton)
+        // flareTradeButton.setOnClickListener { 
+        //     // Handle button click logic here
+        // }
     }
 }
